7ec6b8af41fafe72ee498343f2f2df7d6c3f5770
Rui Soares <rui.soares.aeffl@hotmail.com>
2016-02-17 10:58:56 +0000

    segunda message box

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -20,6 +20,10 @@ namespace WindowsFormsApplication1
         private void button1_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Hello");
+            MessageBox.Show("Hello2");
+
+
+
         }
     }
 }
